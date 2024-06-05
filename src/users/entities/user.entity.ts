2590a0c59ad4dcd91dc0485c62642bfff2efc8f5
Max <max@Mac-mini-Max.local>
2024-06-06 00:45:28 +0700

    Added entity fields

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,32 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  firstName: string
+
+  @Column()
+  lastName: string
+
+  @Column()
+  age: number
+
+  @Column()
+  gender: string
+
+  @Column()
+  state: boolean
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+}
