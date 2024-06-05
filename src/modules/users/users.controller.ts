df6e2e84b6c87831fcb1b45a321858dd81d0c671
Max <max@Mac-mini-Max.local>
2024-06-06 02:23:52 +0700

    Added crud user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,42 +1,59 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common'
+import { Controller, Post, Body, Patch, Get } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger'
+import { User } from './entities/user.entity'
+import { MessagePattern } from '@nestjs/microservices'
 
+@ApiBearerAuth()
+@ApiTags('User')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ description: 'Регистрация пользователя' })
+  @ApiResponse({
+    status: 201,
+    description: 'Пользователь успешно зарегестрирован проверьте почту!',
+  })
+  @ApiResponse({ status: 404, description: 'Неудалось создать пользователя' })
+  @ApiResponse({ status: 500, description: 'Внутренняя ошибка сервера' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto)
   }
 
+  @MessagePattern('users:getCountByState')
+  async getCountByState(data: boolean) {
+    return this.usersService.getCountByState(data)
+  }
+
+  @Patch()
+  @ApiOperation({ description: 'Обновление пользователя' })
+  @ApiResponse({
+    status: 201,
+    description: 'Пользователь успешно обновлен!',
+  })
+  @ApiResponse({ status: 404, description: 'Неудалось обновить пользователя' })
+  @ApiResponse({ status: 500, description: 'Внутренняя ошибка сервера' })
+  update(@Body() params: Partial<User>) {
+    return this.usersService.update(params)
+  }
+
   @Get()
-  findAll() {
-    return this.usersService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id)
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto)
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id)
+  @ApiOperation({ description: 'Получение всех пользователей' })
+  @ApiResponse({
+    status: 201,
+    description: 'Получение всех пользователей',
+  })
+  @ApiResponse({ status: 404, description: 'Неудалось получить пользователей' })
+  @ApiResponse({ status: 500, description: 'Внутренняя ошибка сервера' })
+  async getAllUsers() {
+    return this.usersService.getAllUsers()
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,27 +1,33 @@
 import { BadRequestException, Injectable } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
 import { InjectRepository } from '@nestjs/typeorm'
 import { User } from './entities/user.entity'
 import { Repository } from 'typeorm'
+import { MicroTransportService } from 'src/micro-transport/micro-transport.service'
+import { generatePassword } from 'src/utils/GenPass'
+import { hashPassword } from 'src/utils/Bcrypt'
+import * as dayjs from 'dayjs'
+import * as utc from 'dayjs/plugin/utc'
+dayjs.extend(utc)
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly microTransportService: MicroTransportService,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    const emailCheck = await this.userRepository
-      .createQueryBuilder('User')
-      .where('User.email = :email', { email: createUserDto.email })
-      .getOne()
+    const emailCheck = await this.findByEmail(createUserDto.email)
 
-    if (emailCheck)
+    if (emailCheck) {
       throw new BadRequestException(
         'Пользователь с указанной почтой уже существует!',
       )
+    }
+
+    const password = generatePassword()
 
     const user = await this.userRepository.save({
       email: createUserDto.email,
@@ -29,33 +35,57 @@ export class UsersService {
       lastName: createUserDto.lastName,
       age: createUserDto.age,
       gender: createUserDto.gender,
+      password: await hashPassword(password),
     })
 
-    this.microTransportService
+    await this.microTransportService.sendToMicroservice('signUp_sendEmail', {
+      email: createUserDto.email,
+      firstName: createUserDto.firstName,
+      password,
+    })
+
+    await this.microTransportService.sendToMicroservice('createUser', {
+      userId: user.id,
+      createdAt: dayjs().utc().format(),
+      method: 'created',
+    })
+
+    return 'Пользователь успешно зарегестрирован проверьте почту!'
   }
 
-  async findByEmail() {
+  async findByEmail(params: string): Promise<User> {
     const result = await this.userRepository
       .createQueryBuilder('User')
-      .where('User.email = :email', { email: createUserDto.email })
+      .where('User.email = :email', { email: params })
       .getOne()
 
     return result
   }
 
-  findAll() {
-    return `This action returns all users`
+  async getCountByState(params: boolean) {
+    const result = await this.userRepository
+      .createQueryBuilder('User')
+      .where('User.state = :state', { state: params })
+      .getCount()
+
+    return result
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`
+  async update(params: Partial<User>) {
+    const { id, ...data } = params
+
+    await this.userRepository.update(id, data)
+
+    await this.microTransportService.sendToMicroservice('createUser', {
+      userId: id,
+      updatedAt: dayjs().utc().format(),
+      method: 'updated',
+    })
+
+    return 'Пользователь успешно обновлен!'
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`
+  async getAllUsers() {
+    return await this.userRepository.createQueryBuilder('User').getMany()
   }
 }
