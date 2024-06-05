994b83ca004ed7ba627664d5c2464c969b63f21d
Maxim Gladkov <100793717+TheStutzen@users.noreply.github.com>
2024-06-06 02:30:44 +0700

    Revert "Feature/crud api users"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { UsersModule } from './modules/users/users.module'
-import { MicroTransportModule } from './micro-transport/micro-transport.module'
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [UsersModule, MicroTransportModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
