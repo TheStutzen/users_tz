381f33928b735d4ce02f3a33e33e2b0fe6f7bf4b
Max <max@Mac-mini-Max.local>
2024-06-06 03:19:16 +0700

    Fix imports

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './entities/user.entity'
+import { MicroTransportModule } from 'src/micro-transport/micro-transport.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), MicroTransportModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
