d4ac6be0b25d648ada2552aa8582efe498c53f32
Max <max@Mac-mini-Max.local>
2024-06-06 14:40:54 +0700

    Added swagger & pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      enableDebugMessages: true,
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
+  const config = new DocumentBuilder()
+    .setTitle('Users_TZ(backend)')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger', app, document)
+
+  await app.listen(3030)
 }
 bootstrap()
